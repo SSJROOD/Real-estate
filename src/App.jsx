48ff95c8c58f252d8e3cdc24d808155cc9bbfca9
SSJROOD <rkv20@fsu.edu>
2023-12-21 10:25:25 -0500

    first commit starting project, installed and configured tailwind

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const style_string ="text-red-500 ml-2"
+const App = () => {
+  return (
+    <div className={style_string}>App</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./index.css";
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
