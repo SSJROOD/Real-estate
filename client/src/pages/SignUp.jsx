cb3ba35b057edf774f0d3af422419c6bab8aebf0
SSJROOD <rkv20@fsu.edu>
2023-12-22 20:31:00 -0500

    fixed a tailwindcss syntax error

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
     console.log(data);
   };
   return (
-    <div className="p-3 max-w-lg mx-autoc">
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
